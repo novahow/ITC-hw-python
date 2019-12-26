fd88f59347540b2d8429d724af97b5afabbfedc0
Joshuaoneheart <55230684+Joshuaoneheart@users.noreply.github.com>
2019-12-27 01:18:45 +0800

    Fix the encoding problem and I think the project has been finished.

--- main.py
+++ main.py
@@ -8,11 +8,11 @@ if __name__ == '__main__':
     crawler = Crawler()
     contents = crawler.crawl(args.start_date, args.end_date)
     # TODO: write content to file according to spec
-    with open(args.output, "w") as f:
+    with open(args.output, "w",encoding = "UTF-8") as f:
     	for date, title, content in contents:
     		title = title.replace('\n',' ').replace('\r','')
     		title = title.replace('\"','\"\"')
-    		title = title.replace('\n',' ').replace('\r','')
-    		title = title.replace('\"','\"\"')
+    		content = content.replace('\n',' ').replace('\r','')
+    		content = content.replace('\"','\"\"')
     		out_str = f'{str(date)}, "{title}","{content}"\n'
     		f.write(out_str)
